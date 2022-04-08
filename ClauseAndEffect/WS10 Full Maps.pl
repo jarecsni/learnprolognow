f8bb7af8609e60656c4ffda1e9097fa28a284732
Janos Jarecsni <janos.jarecsni@gmail.com>
2022-04-09 00:24:08 +0100

    Fullmap example finished

--- ClauseAndEffect/WS10 Full Maps.pl
+++ ClauseAndEffect/WS10 Full Maps.pl
@@ -2,4 +2,16 @@ sqlist([], []).
 sqlist([X|T], [Y|L]) :- Y is X * X, sqlist(T, L).
 
 sqterm([], []).
-sqterm([X|T],[s(X,Y)|L]) :- Y is X * X, sqterm(T, L).
+sqterm([X|T], [s(X,Y)|L]) :- Y is X * X, sqterm(T, L).
+
+fullmap([], []).
+fullmap([X|T], [Y|L]) :- word(X, Y), fullmap(T, L).
+
+word(the, 17).
+word(cat, 23).
+word(sits, 46).
+word(on, 9).
+word(mat, 2).
+
+% query:
+% fullmap([the, cat, sits, on, the, mat], X).

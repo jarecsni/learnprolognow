e8e703796accf14aa97492389c061843cec0cc3d
Janos Jarecsni <janos.jarecsni@gmail.com>
2022-04-26 08:54:42 +0100

    PP arithmetic examples

---
+++ ProgrammingInProlog/reigns-example.pl
@@ -0,0 +1,12 @@
+reigns(rhodri, 844, 878).
+reigns(anarawd, 878, 916).
+reigns(hywel_dda, 916, 950).
+reigns(lago_ap_idwal, 950, 979).
+reigns(hywel_ap_ieuaf, 979, 985).
+reigns(cadwallon, 985, 986).
+reigns(maredudd, 986, 999).
+
+prince(Name, Year) :- 
+    reigns(Name, FromYear, ToYear),
+    Year >= FromYear,
+    Year =< ToYear.

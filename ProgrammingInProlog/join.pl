ba4d852c91a36ba768128cf1eb9d0f32367a4601
Janos Jarecsni <janos.jarecsni@gmail.com>
2022-06-01 11:24:30 +0100

    doc

--- ProgrammingInProlog/join.pl
+++ ProgrammingInProlog/join.pl
@@ -1,2 +1,15 @@
 append([], L, L).
 append([X|L1], L2, [X|L3]) :- append(L1, L2, L3).
+
+/*
+    append([a,b,c], [d,e,f], [a,b,c,d,e,f]) -> true.
+
+    The way the above code works is this. It will recursively call append, by essentially
+    consuming L1 down to an empty list, where the target list L3 becomes L2, at this
+    point L3 is [d, e, f]. Now as we come back from the recursive stack, the head 
+    elements of L1 get appended to this list in order, like:
+
+        [c] [d,e,f] => [c,d,e,f]
+        [b] [c,d,e,f] => [b,c,d,e,f]
+        [a] [b,c,d,e,f] => [a,b,c,d,e,f]
+*/
--- ProgrammingInProlog/joinstruct.pl
+++ ProgrammingInProlog/joinstruct.pl
@@ -14,8 +14,24 @@ assembly(frontframe, [fork, handles]).
 assembly(hub, [gears, axle]).
 assembly(axle, [bolt, nut]).
 
+/*
+    If first arg is a basicpart, then we return it (in 2nd arg) as a list ([X]).
+*/
 partsof(X, [X]) :- basicpart(X).
+
+/*
+    If first arg is part of an assembly (first clause), then we need to examine
+    the list of parts in that assembly in a recursive way (partsoflist) adding to 
+    the resulting Assembly all the subparts 
+*/
 partsof(Part, Assembly) :- assembly(Part, SubParts), partsoflist(SubParts, Assembly).
+
+/*
+    This is beautiful: traverse the list, by calling recursively the partsoflist on 
+    the ever diminishing tail, while also working out the parts of the head (which itself
+    might result in further recursive calls if P is an assembly), gluing together head parts
+    with the tailparts. 
+*/
 partsoflist([], []).
 partsoflist([P|Tail], Total) :-
     partsof(P, HeadParts),
